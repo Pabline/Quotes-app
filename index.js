f33fc7208df04ea61a538c75e417ebcc5c6293cf
Pablo Martin <pablo.martin.najera@gmail.com>
2017-06-27 20:29:56 +0200

    Changes in config and added remote MongoDB

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 
 const mongoose = require('mongoose')
 const app = require('./app')
-const port = process.env.PORT || 3001;
+const config = require('./config')
 
-mongoose.connect('mongodb://localhost:27017/quotesDB', (err, res) => {
+//mongodb://localhost:27017/quotesDB
+mongoose.connect(config.db, (err, res) => {
   if(err) {
     return console.log(`Error to connect to bbdd: ${err}`);
   };
   console.log('Conexion a la bbdd establecida');
 
-  app.listen(port, () =>{
-    console.log(`API REST running on http://localhost:${port}`);
+  app.listen(config.port, () =>{
+    console.log(`API REST running on http://localhost:${config.port}`);
   })
 })
