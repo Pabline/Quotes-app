13f00ecc5a1942d4f4cfc03a5907f7ef22b49b85
DTTEC\pmarti38 <pmarti38@tragsa.es>
2017-08-31 14:42:30 +0200

    Añadido webComponent de quotes y cambios de estilos.

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const api = require('./routes')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use('/api', api)
