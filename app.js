c16512b21818981aca579075afd35c698a43cd98
DTTEC\pmarti38 <pmarti38@tragsa.es>
2017-10-19 14:46:50 +0200

    Cambios en login interface y en el buscador

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const api = require('./routes')
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
